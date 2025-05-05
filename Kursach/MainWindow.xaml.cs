5456483f5eb7b2e3d52ec76129a0f288801a60e3
Ann <melnychuk.anna1123@vu.cdu.edu.ua>
2025-05-06 01:02:30 +0300

    Добавьте файлы проекта.

---
+++ Kursach/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kursach
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddProductBox_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel row = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+
+            TextBox productBox = new TextBox
+            {
+                Width = 200, Height = 25, FontSize = 15,
+                Margin = new Thickness(25,10,52,5)
+            };
+
+            TextBox minBox = new TextBox
+            {
+                Width = 100, Height = 25, FontSize = 15,
+                Margin = new Thickness(25, 10, 48, 5)
+            };
+
+            TextBox maxBox = new TextBox
+            { 
+                Width = 100, Height = 25, FontSize = 15,
+                Margin = new Thickness(46, 10, 0, 5)
+            };
+
+            row.Children.Add(productBox);
+            row.Children.Add(minBox);
+            row.Children.Add(maxBox);
+
+            ProductListPanel.Children.Add(row);
+        }
+    }
+}
+
